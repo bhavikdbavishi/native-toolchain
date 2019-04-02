bhavikdbavishi/native-toolchain

commit db301f6b992d36ea315c037fde954e01340be1e2

Include python-dev in all build images
Thrift doesn't fail the build if it fails to find the python development
headers, it instead silently builds without support for the
TBinaryProtocolAccelerated class. This commit adds python-dev to the
missing docker images and adds a couple of assertions to ensure we fail
the build whenever this happens.

Change-Id: Iac6186317b943e3bb7609c9309d7bfdde245e4d8
Reviewed-on: http://gerrit.cloudera.org:8080/12882
Reviewed-by: Tim Armstrong <tarmstrong@cloudera.com>
Tested-by: Tim Armstrong <tarmstrong@cloudera.com>

source/thrift/build.sh
@@ -66,8 +66,9 @@ if needs_build_package ; then
 
 
   # LEXLIB= is a Workaround /usr/lib64/libfl.so: undefined reference to `yylex'
+  PY_PREFIX="${LOCAL_INSTALL}"/python
   PATH="${BISON_ROOT}"/bin:"${PATH}" \
-    PY_PREFIX="${LOCAL_INSTALL}"/python \
+    PY_PREFIX="${PY_PREFIX}" \
     wrap ./configure \
     LEXLIB= \
     --with-pic \
@@ -114,5 +115,9 @@ if needs_build_package ; then
     --with-java=no --with-php=no --prefix="${LOCAL_INSTALL}" \
     --with-thriftpath="${LOCAL_INSTALL}" ${OPENSSL_ARGS}
   wrap make -j"${BUILD_THREADS}" install
+
+  # Ensure that we've compiled the fastbinary shared object
+  PYTHONPATH=$(find $PY_PREFIX -type d -name site-packages) \
+    python -c 'import thrift.protocol.fastbinary'
   finalize_package_build "${PACKAGE}" "${PACKAGE_VERSION}"
 fi

docker/all/assert-dependencies-present.py
@@ -19,12 +19,14 @@
 
 import distutils.core  # noqa: F401
 import distutils.spawn
+import distutils.sysconfig
 import platform
 import subprocess
 import logging
 import os
 import re
 import shutil
+import sys
 
 LOG = logging.getLogger('assert-dependencies')
 
@@ -40,6 +42,13 @@ def check_output(cmd):
 def regex_in_list(regex, l):
   cr = re.compile(regex)
   return any(map(cr.match, l))
+
+
+def check_python_headers_present():
+  # Causes thrift to fail silently, so ensure this is present
+  include = os.path.join(distutils.sysconfig.get_python_inc(), 'Python.h')
+  LOG.info('Checking if %s exists', include)
+  assert os.path.isfile(include)
 
 
 def check_libraries():
@@ -129,6 +138,7 @@ def main():
   git_clone_works_with_https()
   check_path()
   check_openssl_version()
+  check_python_headers_present()
   check_aws_works()
   check_mvn_works()
 

docker/debian7.df
@@ -20,6 +20,7 @@ RUN apt-get update && apt-get install -y autoconf \
       openjdk-7-jdk \
       pigz \
       pkg-config \
+      python-dev \
       rsync \
       texinfo \
       unzip \

docker/debian8.df
@@ -20,6 +20,7 @@ RUN apt-get update && apt-get install -y autoconf \
       openjdk-7-jdk \
       pigz \
       pkg-config \
+      python-dev \
       rsync \
       texinfo \
       unzip \

docker/ubuntu1204.df
@@ -18,6 +18,7 @@ RUN apt-get update && apt-get install -y autoconf \
       openjdk-7-jdk \
       pigz \
       pkg-config \
+      python-dev \
       rsync \
       texinfo \
       unzip \

docker/ubuntu1404.df
@@ -18,6 +18,7 @@ RUN apt-get update && apt-get install -y autoconf \
       openjdk-7-jdk \
       pigz \
       pkg-config \
+      python-dev \
       python \
       rsync \
       texinfo \

docker/ubuntu1604.df
@@ -19,6 +19,7 @@ RUN apt-get update && apt-get install -y autoconf \
       pigz \
       pkg-config \
       python \
+      python-dev \
       rsync \
       texinfo \
       unzip \

docker/ubuntu1804.df
@@ -21,6 +21,7 @@ RUN apt-get update && apt-get install -y autoconf \
       pkg-config \
       python \
       python3-distutils \
+      python-dev \
       rsync \
       texinfo \
       unzip \

commit a784a7117a14b2459bfa81a3139b974c79192d52

Check for thirft.protocol.fastbinary in both lib and lib64
db301f6b992d36ea315c037fde954e01340be1e2 introduced an assertion on
the thrift build script which ensures that thrift.protocol.fastbinary
gets compiled in thrift's python bindings, however in some distros, the
shared object is placed on $PY_PREFIX/lib64 (instead of $PY_PREFIX/lib)
so we check in both locations.

Change-Id: Ib2294eff2f945af94de0345322f5496777fb1b08

source/thrift/build.sh
@@ -117,7 +117,8 @@ if needs_build_package ; then
   wrap make -j"${BUILD_THREADS}" install
 
   # Ensure that we've compiled the fastbinary shared object
-  PYTHONPATH=$(find $PY_PREFIX -type d -name site-packages) \
+  # Some distros place site-packages on lib and others do so in lib64
+  PYTHONPATH=$(find $PY_PREFIX -type d -name 'site-packages' -type d|tr '\n' ':') \
     python -c 'import thrift.protocol.fastbinary'
   finalize_package_build "${PACKAGE}" "${PACKAGE_VERSION}"
 fi